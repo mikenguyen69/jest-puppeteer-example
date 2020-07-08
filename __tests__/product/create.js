ea8867828ee027103743fa4812e5b351e6e95e88
Mike Nguyen <Mike.Nguyen@solnet.co.nz>
2020-07-08 14:58:24 +1200

    fix the default test..

--- __tests__/product/create.js
+++ __tests__/product/create.js
@@ -20,3 +20,25 @@ const create_product_setup = {
 //const run =  require('../../execute');
 //run(create_product_setup);
 
+const timeout = 5000
+
+describe(
+  '/ (Home Page)',
+  () => {
+    let page
+    beforeAll(async () => {
+      page = await global.__BROWSER__.newPage()
+      await page.goto('https://google.com')
+    }, timeout)
+
+    afterAll(async () => {
+      await page.close()
+    })
+
+    it('should load without error', async () => {
+      let text = await page.evaluate(() => document.body.textContent)
+      expect(text).toContain('google')
+    })
+  },
+  timeout
+)

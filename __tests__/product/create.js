6f2e2a23d5534e286becd81b9935269e9035c187
Mike Nguyen <Mike.Nguyen@solnet.co.nz>
2020-07-08 13:00:52 +1200

    Create test runner and separate them from the test cases configuration

--- __tests__/product/create.js
+++ __tests__/product/create.js
@@ -1,58 +1,24 @@
-const timeout = 15000;
-const steps = [
-    {type: 'text', focus: 'input[name=name]', value: 'Test item only'},
-    {type: 'text', focus: 'input[name=price]', value: '19.00'},
-    {type: 'text', focus: 'textarea[name=description]', value: 'this is for testing!'},
-    {type: 'file', focus: 'input[name=media]', value: '/Users/mike.nguyen/Downloads/set1.jpeg'},
-]
 
-async function handleTextInput(page, input) {
-    await page.focus(input.focus);
-    await page.keyboard.type(input.value);
+const testRunner = require('../../execute');
+
+const create_product_setup = {
+    steps: [
+        {type: 'text', focus: 'input[name=name]', value: 'Test item only'},
+        {type: 'text', focus: 'input[name=price]', value: '19.00'},
+        {type: 'text', focus: 'textarea[name=description]', value: 'this is for testing!'},
+        {type: 'file', focus: 'input[name=media]', value: '/Users/mike.nguyen/Downloads/set1.jpeg'},
+        {type: 'submit', focus: 'button[type=submit]', value: ''}
+    ],
+    url: 'http://localhost:3000/create',
+    describe: '/create (Product)',
+    tests: [
+        { it: 'should load without error', asserts: [
+            { condition: 'toContain', text: 'Success!Your product has been posted', } ,
+            { condition: 'not.toContain', text: 'Opps!Server error in creating product' }
+        ]}
+    ],
+    delay: 10000
 }
 
-async function handleFileInput(page, input) {
-    const fileInput = await page.$(input.focus);
-    await fileInput.uploadFile(input.value);
-}
+testRunner.run(create_product_setup);
 
-describe(
-    '/create (Product)',
-    () => {
-        let page 
-        beforeAll(async () => {
-            page = await global.__BROWSER__.newPage()
-
-            await page.goto('http://localhost:3000/create')
-            
-
-            for(var i=0; i < steps.length; i++) {
-                var input = steps[i];
-
-                if (input.type === 'text') {
-                    await handleTextInput(page, input);
-                }
-                else if (input.type === 'file') {
-                    await handleFileInput(page, input);
-                }
-            }
-
-            const submitBtn = await page.$('button[type=submit]');
-            await submitBtn.click();
-
-            await page.waitFor(10000);
-
-        }, timeout)
-
-        afterAll(async () => {
-            await page.close()
-        })
-
-        it('should load without error', async () => {
-            let text = await page.evaluate(() => document.body.textContent)
-            //console.log(text);
-            expect(text).toContain('Success!Your product has been posted');
-            expect(text).not.toContain('Opps!Server error in creating product');
-        })
-    }
-)
---
+++ execute.js
@@ -0,0 +1,72 @@
+
+module.exports.run = function(testSetup) {
+    const timeout = 15000;
+
+    describe(
+        testSetup.describe,
+        () => {
+            let page 
+            beforeAll(async () => {
+                page = await global.__BROWSER__.newPage()
+    
+                await page.goto(testSetup.url)
+                
+    
+                for(var i=0; i < testSetup.steps.length; i++) {
+                    var input = testSetup.steps[i];
+    
+                    if (input.type === 'text') {
+                        await handleTextInput(page, input);
+                    }
+                    else if (input.type === 'file') {
+                        await handleFileInput(page, input);
+                    }
+                    else if (input.type === 'submit') {
+                        await handleSubmit(page, input);
+                    }
+                }
+    
+                await page.waitFor(testSetup.delay);
+    
+            }, timeout)
+    
+            afterAll(async () => {
+                await page.close()
+            })
+    
+            // Assessment
+            for(var i=0; i < testSetup.tests.length; i++) {
+                var test = testSetup.tests[i];
+    
+                it(test.it, async () => {
+                    let text = await page.evaluate(() => document.body.textContent)
+                    //console.log(text);
+                    
+                    test.asserts.forEach(function(test) {
+                        if (test.condition === 'toContain') {
+                            expect(text).toContain(test.text);
+                        }
+                        else if (test.condition === 'not.toContain') {
+                            expect(text).not.toContain(test.text);
+                        }
+                    });
+                })
+            }
+        }
+    )
+}
+
+async function handleTextInput(page, input) {
+    await page.focus(input.focus);
+    await page.keyboard.type(input.value);
+}
+
+async function handleFileInput(page, input) {
+    const fileInput = await page.$(input.focus);
+    await fileInput.uploadFile(input.value);
+}
+
+async function handleSubmit(page, input) {
+    const submitBtn = await page.$(input.focus);
+    await submitBtn.click();
+}
